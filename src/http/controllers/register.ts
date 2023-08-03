01c43855b0224f216f2f73a42432c0c838c819f3
Pedro Cruz <54217967+Hyakutak@users.noreply.github.com>
2023-08-03 08:54:54 -0300

    Change const verify email user

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -14,13 +14,13 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
     const password_hash = await hash(password, 6);
 
-    const userWithSameEmail = await prisma.user.findUnique({
+    const ExistEmailUser = await prisma.user.findUnique({
         where: {
             email,
         }
     });
 
-    if(userWithSameEmail) {
+    if(ExistEmailUser) {
         return reply.status(409).send();
     }
 
